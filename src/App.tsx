c8a840ff16a00acef1c95c2708e1a99e689b8d99
kalvin <kalvinyu0@hotmail.com>
2025-08-14 20:51:59 +0200

    start

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import  ImageEditor from './imageEd';
+
+
+
+export function App() {
+  return (
+    <div className="max-w-7xl mx-auto p-8 text-center relative z-10">
+      <div className="flex justify-center items-center gap-8 mb-8">
+      </div>
+
+      <h1 className="text-5xl font-bold my-4 leading-tight">Bun + React</h1>
+      <ImageEditor/>
+    </div>
+  );
+}
+
+export default App;
